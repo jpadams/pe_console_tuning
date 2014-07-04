dd75b4308bcd86214ab720c3edfdfc2982735512
Jeremy Adams <jeremy@puppetlabs.com>
2014-07-04 13:31:43 -0700

    added validation and show lm certnames

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,12 @@
 class pe_console_tuning (
   $console_timeout = 1200,
-  $facts_array = ["architecture", "domain", "fqdn", "hardwaremodel", "hostname", "ipaddress", "is_virtual", "manufacturer", "memorytotal", "operatingsystem", "operatingsystemrelease", "physicalprocessorcount", "processorcount", "productname", "puppetversion"] 
+  $facts_array = ["architecture", "domain", "fqdn", "hardwaremodel", "hostname", "ipaddress", "is_virtual", "manufacturer", "memorytotal", "operatingsystem", "operatingsystemrelease", "physicalprocessorcount", "processorcount", "productname", "puppetversion"],
+  $show_certnames_live_mgmt = false
 ) {
+
+  if ! is_integer($console_timeout) { fail() }
+  validate_array($facts_array)
+  validate_bool($show_certnames_live_mgmt)
 
   file_line { 'console timeout setting 1 of 2':
     path  => '/etc/puppetlabs/rubycas-server/config.yml',
@@ -22,6 +27,14 @@ class pe_console_tuning (
     path  => '/opt/puppet/share/live-management/public/javascripts/app_controller.js',
     match => "PuppetEnterpriseConsole.set\('factNames',",
     line  => "    PuppetEnterpriseConsole.set('factNames', ${facts});",
+  }
+  
+  if $show_certnames_live_mgmt == true {
+    file_line { 'show certnames by default in  Live Management':
+      path  => '/opt/puppet/share/live-management/views/index.erb',
+      match => '<div class="node-list" style="display:',
+      line  => '      <div class="node-list" style="display: block">',
+    }
   }
 
 }
